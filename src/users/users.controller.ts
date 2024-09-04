66837ef36463dff97056c859f365d11d8d40ad15
Rianlucas7 <rianlucas592@gmail.com>
2024-09-04 15:27:05 -0300

    feat: adds a validation to the page and limit parameters of the get users route
    checks that page and limit really are of type number and adds a limit of 50 users per search

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,18 +13,38 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
-    return this.usersService.findAll(Number(page), Number(limit));
+  findAll(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ) {
+    const pageNumber = Number(page);
+    const limitNumber = Number(limit);
+
+    if (isNaN(pageNumber) || pageNumber <= 0) {
+      throw new HttpException(
+        'O valor de página deve ser um número positivo.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (isNaN(limitNumber) || limitNumber <= 0 || limitNumber > 50) {
+      throw new HttpException(
+        'O valor de limite deve ser um número positivo e não deve exceder 50.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return this.usersService.findAll(pageNumber, limitNumber);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    const numericId = Number(id);
+    const IdNumber = Number(id);
 
-    if (isNaN(numericId)) {
+    if (isNaN(IdNumber)) {
       throw new HttpException('Id inválido', HttpStatus.BAD_REQUEST);
     }
 
-    return this.usersService.findOne(numericId);
+    return this.usersService.findOne(IdNumber);
   }
 }
